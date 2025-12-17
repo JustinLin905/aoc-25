3c81155c5af4346de3007a5bc2ee32518b28589c
Justin Lin <tojustinlin@gmail.com>
2025-12-17 16:57:33 -0500

    q4p1

---
+++ q4/q4.cc
@@ -0,0 +1,76 @@
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+// Constants
+constexpr int ADJ_THRESHOLD = 4;
+
+// Helper fn to count adjacent tiles that are rolls
+int countAdjTiles(const vector<vector<char>> &graph, int r, int c) {
+  static constexpr int dirs[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                                     {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+  int n = graph.size();
+  int m = graph[r].size();
+
+  int count = 0;
+
+  for (auto &dir : dirs) {
+    int row = r + dir[0];
+    int col = c + dir[1];
+
+    if (0 <= row && row < n && 0 <= col && col < m && graph[row][col] == '@') {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << "Usage: " << argv[0] << " <filename>" << endl;
+    return 1;
+  }
+
+  string line;
+  ifstream in(argv[1]);
+  int totalRollCount = 0;
+
+  vector<vector<char>> graph;
+
+  if (in.is_open()) {
+    // Ingest all lines to start
+    while (getline(in, line)) {
+      // Split line into chars and add to graph
+      vector<char> newRow;
+      for (char ch : line) {
+        newRow.push_back(ch);
+      }
+      graph.push_back(newRow);
+    }
+    in.close();
+  } else {
+    cout << "Unable to open file error";
+    return 1;
+  }
+
+  // Check each tile in graph
+  for (int r = 0; r < graph.size(); r++) {
+    for (int c = 0; c < graph[r].size(); c++) {
+      if (graph[r][c] == '@' && countAdjTiles(graph, r, c) < ADJ_THRESHOLD) {
+        totalRollCount++;
+      }
+    }
+  }
+
+  cout << "Output: " << totalRollCount << endl;
+  return 0;
+}
