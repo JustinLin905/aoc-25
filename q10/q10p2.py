34c14340f2156ee8a0956ccb33204d3bd529ef88
Justin Lin <tojustinlin@gmail.com>
2026-05-05 15:49:12 -0400

    Update q10p2.py

--- q10/q10p2.py
+++ q10/q10p2.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+import os
 import sys
 from functools import cache
 from collections import defaultdict, Counter, deque
 import z3
 
-D = sys.stdin.read()
+current_dir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(current_dir, "q10.txt"), "r") as f:
+    D = f.read()
+
 p2 = 0
 for line in D.splitlines():
     words = line.split()
@@ -15,34 +19,39 @@ for line in D.splitlines():
         indices = [int(x) for x in button[1:-1].split(",")]
         button_indices.append(indices)
 
-    # solve Ax = B
-    # where A = effect of each button
-    # x = how many times we press each button
-    # B = goal state
-    # minimize(sum(X))
     joltage_string = words[-1]
     joltages = [int(x) for x in joltage_string[1:-1].split(",")]
-    V = []
+
+    # Create Z3 integers for each button
+    x = []
     for i in range(len(buttons)):
-        V.append(z3.Int(f"B{i}"))
+        x.append(z3.Int(f"B{i}"))
+
+    # Construct the requirements
     EQ = []
     for i in range(len(joltages)):
         terms = []
         for j in range(len(buttons)):
             if i in button_indices[j]:
-                terms.append(V[j])
+                # Find each button that affects this joltage index
+                terms.append(x[j])
+
+        # The sum of the effects of the buttons that affect this joltage index must equal the target joltage
+        # For example, if B4 and B5 both affect index 0, this states:
+        # B4 + B5 == 3
         eq = sum(terms) == joltages[i]
         EQ.append(eq)
+
     o = z3.Optimize()
-    o.minimize(sum(V))
+    o.minimize(sum(x))
     for eq in EQ:
-        o.add(eq)
-    for v in V:
-        o.add(v >= 0)
+        o.add(eq)  # Each counter must hit its target
+    for v in x:
+        o.add(v >= 0)  # Buttons can't be pressed a negative number of times
     assert o.check()
+
     M = o.model()
     for d in M.decls():
-        # print(d.name(), M[d])
         p2 += M[d].as_long()
 
 print(p2)
